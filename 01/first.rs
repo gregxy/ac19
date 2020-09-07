6db5b8433f65f0982855734611a9c0f34dcc912f
Greg Yang <gregyang@outlook.com>
2020-09-07 16:20:57 -0700

    Day01: use iter

--- 01/first.rs
+++ 01/first.rs
@@ -1,10 +1,16 @@
+use std::error::Error;
+use std::fs::File;
 use std::io::BufRead;
+use std::io::BufReader;
 
-fn main() {
-  let mut sum : i32 = 0;
-  for line in std::io::stdin().lock().lines() {
-    let num = line.unwrap().parse::<i32>().unwrap();
-    sum += num / 3 - 2
-  }
-  println!("{}", sum)
+fn main() -> Result<(), Box<dyn Error>> {
+    let f = File::open("input.txt")?;
+    let sum = BufReader::new(f)
+        .lines()
+        .map(|line| line.unwrap().parse::<i32>().unwrap() / 3 - 2)
+        .sum::<i32>();
+
+    println!("{}", sum);
+
+    return Ok(());
 }
--- 01/second.rs
+++ 01/second.rs
@@ -1,13 +1,27 @@
+use std::error::Error;
+use std::fs::File;
 use std::io::BufRead;
+use std::io::BufReader;
 
-fn main() {
-  let mut sum : i32 = 0;
-  for line in std::io::stdin().lock().lines() {
-    let mut fuel = line.unwrap().parse::<i32>().unwrap() / 3 - 2;
+fn cal(v: i32) -> i32 {
+    let mut sum: i32 = 0;
+    let mut fuel = v / 3 - 2;
     while fuel > 0 {
-      sum += fuel;
-      fuel = fuel / 3 - 2;
+        sum += fuel;
+        fuel = fuel / 3 - 2;
     }
-  }
-  println!("{}", sum)
+
+    return sum;
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let f = File::open("input.txt")?;
+    let sum = BufReader::new(f)
+        .lines()
+        .map(|line| cal(line.unwrap().parse::<i32>().unwrap()))
+        .sum::<i32>();
+
+    println!("{}", sum);
+
+    return Ok(());
 }
