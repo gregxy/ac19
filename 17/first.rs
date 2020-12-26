52a99b4c79f416e11b4bdd86b70eca1420d4263e
Greg Yang <gregyang@outlook.com>
2020-12-26 14:48:26 -0800

    day 17

---
+++ 17/first.rs
@@ -0,0 +1,172 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+fn main() {
+    let prog = std::fs::read_to_string("input.txt")
+        .unwrap()
+        .trim()
+        .split(',')
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+
+    let mut scaffolding : HashSet<(i64, i64)> = HashSet::new();
+    let mut computer = Computer::new(&prog);
+    let mut x :i64 = 0;
+    let mut y :i64 = 0;
+    loop {
+        let v = computer.run(0);
+        if v <= 0 {
+            break;
+        }
+
+        print!("{}", char::from(v as u8));
+
+        if v == 10 {
+            x = 0;
+            y += 1;
+            continue;
+        }
+
+        if v != 46 {
+            scaffolding.insert((x, y));
+        }
+        x += 1;
+    }
+
+    let mut sum : i64 = 0;
+    for (x, y) in scaffolding.iter() {
+        if scaffolding.contains(&(*x-1, *y)) &&
+            scaffolding.contains(&(*x+1, *y)) &&
+            scaffolding.contains(&(*x, *y+1)) &&
+            scaffolding.contains(&(*x, *y-1)) {
+            sum += x * y;
+        }
+    }
+
+    println!("{}", sum);
+}
+
+struct Computer {
+    index: usize,
+    base: i64,
+    mem: HashMap<usize, i64>,
+}
+
+impl Computer {
+    fn new(prog: &Vec<i64>) -> Computer {
+        let mut computer = Computer {
+            index: 0,
+            base: 0,
+            mem: HashMap::new(),
+        };
+
+        for (index, v) in prog.iter().enumerate() {
+            computer.mem.insert(index, *v);
+        }
+
+        return computer;
+    }
+
+    fn fetch(&self, address: usize) -> i64 {
+        if let Some(v) = self.mem.get(&address) {
+            return *v;
+        }
+
+        return 0i64;
+    }
+
+    fn decode(&self) -> (i64, usize, usize, usize) {
+        let opv = self.fetch(self.index);
+
+        let c = (opv / 100) % 10;
+        let d = (opv / 1000) % 10;
+        let e = (opv / 10000) % 10;
+        let op = opv % 100;
+
+        let la = if c == 1 {
+            self.index + 1
+        } else if c == 0 {
+            self.fetch(self.index + 1) as usize
+        } else {
+            (self.base + self.fetch(self.index + 1)) as usize
+        };
+
+        let ra = if d == 1 {
+            self.index + 2
+        } else if d == 0 {
+            self.fetch(self.index + 2) as usize
+        } else {
+            (self.base + self.fetch(self.index + 2)) as usize
+        };
+
+        let ta = if e == 1 {
+            self.index + 3
+        } else if e == 0 {
+            self.fetch(self.index + 3) as usize
+        } else {
+            (self.base + self.fetch(self.index + 3)) as usize
+        };
+
+        return (op, la, ra, ta);
+    }
+
+    fn run(&mut self, input: i64) -> i64 {
+        loop {
+            let (op, la, ra, ta) = self.decode();
+
+            match op {
+                1 | 2 => {
+                    if op == 1 {
+                        self.mem.insert(ta, self.fetch(la) + self.fetch(ra));
+                    } else {
+                        self.mem.insert(ta, self.fetch(la) * self.fetch(ra));
+                    }
+                    self.index += 4;
+                }
+                3 => {
+                    self.mem.insert(la, input);
+                    self.index += 2;
+                }
+                4 => {
+                    let out = self.fetch(la);
+                    self.index += 2;
+                    return out;
+                }
+                5 | 6 => {
+                    self.index = match (op == 5, self.fetch(la) != 0) {
+                        (true, true) => self.fetch(ra) as usize,
+                        (true, false) => self.index + 3,
+                        (false, true) => self.index + 3,
+                        (false, false) => self.fetch(ra) as usize,
+                    }
+                }
+                7 | 8 => {
+                    if op == 7 {
+                        let v: i64 = if self.fetch(la) < self.fetch(ra) {
+                            1
+                        } else {
+                            0
+                        };
+                        self.mem.insert(ta, v);
+                    } else {
+                        let v: i64 = if self.fetch(la) == self.fetch(ra) {
+                            1
+                        } else {
+                            0
+                        };
+                        self.mem.insert(ta, v);
+                    }
+                    self.index += 4;
+                }
+                9 => {
+                    self.base += self.fetch(la);
+                    self.index += 2;
+                }
+                99 => break,
+                _ => panic!("Unexpected op code"),
+            }
+        }
+
+        return -1;
+    }
+}
